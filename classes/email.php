074a3f169ec991a88761f7b03c1896b279e050d7
pedromael <pedmanue14@gmail.com>
2024-07-09 20:28:09 +0100

    Update email.php

--- classes/email.php
+++ classes/email.php
@@ -1,18 +1,8 @@
 <?php
-<<<<<<< HEAD
-class sig_in extends conexao{
-    public $dados;
-    public function __construct(){
-        parent::__construct();
-    }
-    public function enviar($dados){
-        return true;
-=======
 final class email extends AnotherClass
 {
     public function enviar() {
         return 0;
->>>>>>> 4aa5271c93be6c53c74f31ac8fc62c87e6c5569f
     }
 }
 
